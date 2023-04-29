967b4095be09e1f6690f1c75de80a5851e278998
Alisher Kasimov <alisherkasimov1994@gmail.com>
2023-04-29 17:04:54 +0500

    Refactoring...

--- ui.py
+++ ui.py
@@ -12,40 +12,28 @@ class MainWindow (QWidget):
         super().__init__()
         self.db = Database()
 
-        self.grid = QGridLayout()
+        self.grid = QVBoxLayout()
         self.render_all()
         self.setLayout(self.grid)
     
     def render_all(self):
         data = self.db.get_all_tasks()
 
-        c = [(x, 0, 1) for x in range(len(data))]
-
-        for row, c in zip(data, c):
+        for row in data:
             check = QCheckBox()
-            check.setText(str(row[0]))
             check.clicked.connect(self.make_done)
-            if row[4]:
+            check.setText(str(row[0]) + ". " + row[1])
+            if row[4] == 1:
                 check.setChecked(True)
-                if row[3] == 0:
-                    task.setStyleSheet("font-weight: 700;text-decoration:line-through;")
-                else:
-                    task.setStyleSheet("color: #333; font-size: 10px;text-decoration:line-through;")
-            self.grid.addWidget(check, c[0], c[1])
-
-            task = QLabel(str(row[1]))
-            if row[3] == 0:
-                task.setStyleSheet("font-weight: 700;")
-            else:
-                task.setStyleSheet("color: #333; font-size: 10px")
-
-            self.grid.addWidget(task, c[0], c[2])
+                check.setStyleSheet("text-decoration:line-through;")
+                check.setCheckable(False)
+            self.grid.addWidget(check)
+            
             
     
-    def make_done(self, id):
+    def make_done(self):
         sender = self.sender()
-        print(sender.text())
 
-        self.db.make_done(int(sender.text()))
-        self.grid = QGridLayout()
-        self.render_all()
+        self.db.make_done(int(sender.text().split(".")[0]))
+        sender.setStyleSheet("text-decoration:line-through;")
+        sender.setCheckable(False)
